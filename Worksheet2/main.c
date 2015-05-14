ed489f845252e137574ab652c74965e9e7068499
Wei Ni <paratroop@gmail.com>
2015-05-14 11:24:57 +0200

    add .gitignore and update main.c

--- Worksheet2/main.c
+++ Worksheet2/main.c
@@ -8,10 +8,34 @@
 #include "boundary.h"
 
 int main(int argc, char *argv[]){
+
+	double *collideField=NULL;
+	double *streamField=NULL;
+	int *flagField=NULL;
+	int xlength;
+	double tau;
+	double velocityWall[3];
+	int timesteps;
+	int timestepsPerPlotting;
+	readParameters(&xlength,&tau,&velocityWall,timesteps,timestepsPerPlotting,argc, argv);
+	// TODO: initialise pointers here!
+	initialiseFields(collideField,streamField,flagField,xlength);
+	for(int t = 0; t < timesteps; t++){
+		double *swap=NULL;
+		doStreaming(collideField,streamField,flagfield,xlength);
+		swap = collideField;
+		collideField = streamField;
+		streamField = swap;
+		doCollision(collideField,flagfield,&tau,xlength);
+		treatBoundary(collideField,flagfield,velocityWall,xlength);
+		if (t%timestepsPerPlotting==0){
+			writeVtkOutput(collideField,flagfield,argv,t,xlength);
+		}
+	}
+
   /* TODO */
 
-
-  return 0;
+  	return 0;
 }
 
 #endif
