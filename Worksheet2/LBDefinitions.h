0957792a57d0259be712dff86f6456524dd1f418
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2015-05-15 23:54:18 +0200

    done LBDef., added initializ. in main

--- Worksheet2/LBDefinitions.h
+++ Worksheet2/LBDefinitions.h
@@ -1,9 +1,20 @@
 #ifndef _LBDEFINITIONS_H_
 #define _LBDEFINITIONS_H_
 
-  static const int LATTICEVELOCITIES[19][3];
-  static const double LATTICEWEIGHTS[19];
-  static const double C_S;
+#include <math.h>
+
+  static const int LATTICEVELOCITIES[19][3] = {{0, -1.0, -1.0}, {-1.0, 0, -1.0}, {0, 0, -1.0}, {1.0, 0, -1.0}, {0, 1.0, -1.0}, {-1.0, -1.0, 0}, {0, -1.0, 0},
+						{1.0, -1.0, 0}, {-1.0, 0, 0}, {0, 0, 0}, {1.0, 0, 0}, {-1.0, 1.0, 0}, {0, 1.0, 0}, {1.0, 1.0, 0},
+						{0, -1.0, 1.0}, {-1.0, 0, 1.0}, {0, 0, 1.0}, {1.0, 0, 1.0}, {0, 1.0, 1.0}};
+  static const double LATTICEWEIGHTS[19] = {0.027778, 0.027778, 0.055556, 0.027778, 0.027778, 0.027778, 0.055556, 0.027778, 0.055556, 0.33333,
+						0.055556, 0.027778, 0.055556, 0.027778, 0.027778, 0.027778, 0.055556, 0.027778, 0.027778};
+  static const double C_S = 0.57735;
+
+
+
+  static const int FLUID = 0;
+  static const int NO_SLIP = 1;
+  static const int MOVING_WALL = 2;
 
 #endif
 
--- Worksheet2/main.c
+++ Worksheet2/main.c
@@ -6,34 +6,52 @@
 #include "initLB.h"
 #include "visualLB.h"
 #include "boundary.h"
+#include "LBDefinitions.h"
 
 int main(int argc, char *argv[]){
 
-	double *collideField=NULL;
-	double *streamField=NULL;
-	int *flagField=NULL;
+	double *collideField = NULL;
+	double *streamField = NULL;
+	int *flagField = NULL;
+	double *swap = NULL;
+
 	int xlength;
 	double tau;
 	double velocityWall[3];
 	int timesteps;
 	int timestepsPerPlotting;
-	readParameters(&xlength,&tau,velocityWall,&timesteps,&timestepsPerPlotting,argc,argv);
-	// TODO: initialise pointers here!
-	initialiseFields(collideField,streamField,flagField,xlength);
-	for(int t = 0; t < timesteps; t++){
-		double *swap=NULL;
-		doStreaming(collideField,streamField,flagField,xlength);
+
+	readParameters ( &xlength, &tau, velocityWall, &timesteps, &timestepsPerPlotting, argc, argv );
+
+	int len = (xlength + 2)*(xlength + 2)*(xlength + 2);
+
+	// TODO: initialise space for pointers
+	collideField = calloc ( 19*len, sizeof(double) );
+	streamField = calloc ( 19*len, sizeof(double) );
+	flagField = calloc ( len, sizeof(int) );
+
+	initialiseFields ( collideField, streamField, flagField, xlength );
+
+	for (int t = 0; t < timesteps; t++){
+		doStreaming ( collideField, streamField, flagField, xlength );
+
 		swap = collideField;
 		collideField = streamField;
 		streamField = swap;
-		doCollision(collideField,flagField,&tau,xlength);
-		treatBoundary(collideField,flagField,velocityWall,xlength);
+
+		doCollision ( collideField, flagField, &tau, xlength);
+
+		treatBoundary ( collideField, flagField, velocityWall, xlength);
+
 		if (t%timestepsPerPlotting==0){
 			//writeVtkOutput(collideField,flagField,argv,t,xlength);
 		}
 	}
 
   /* TODO */
+	free ( collideField );
+	free ( streamField );
+	free ( flagField );
 
   	return 0;
 }
