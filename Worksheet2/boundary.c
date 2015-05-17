65c39e4c9cb5e6db587f93e56a437767e3cedabf
ZhibinCH <sebastianbingo@gmail.com>
2015-05-17 14:55:49 +0200

    Update boundary.c

--- Worksheet2/boundary.c
+++ Worksheet2/boundary.c
@@ -18,7 +18,10 @@ void treatBoundary(double *collideField, int* flagField,
                     dx = LATTICEVELOCITIES[i][0];
                     dy = LATTICEVELOCITIES[i][1];
                     dz = LATTICEVELOCITIES[i][2];
-                    
+                    /*Check whether the i th direction faces to the fluid particles or not
+                     if yes, the BC will be concerned, we consider 3 planes, namely y-z, x-z and x-y.
+                    due to the symetric property, it will end up with 6 surfaces (as we expected)
+                     futhermore, 5 of them is no-slipe, 1 is moving wall*/
                     /* y-z plane*/
                     if (x+dx > 0 && x+dx< N+1 &&
                         y+dy > 0 && y+dy< N+1 &&
@@ -27,7 +30,7 @@ void treatBoundary(double *collideField, int* flagField,
                         finv = collideField[Q *((z+dz)*(N+2)*(N+2) + (y+dy)*(N+2)+x+dx)+ Q-i-1];
                         collideField[Q *( z*(N+2)*(N+2) + y*(N+2)+ x)+ i] =finv;
                     }
-                    /*x-z plane swap x and y*/
+                    /*x-z plane swap x and y,the IF loop is therefore written within the same iteration*/
                     if (y+dx > 0 && y+dx < N + 1 &&
                         x+dy > 0 && x+dy < N + 1 &&
                         z+dz > 0 && z+dz < N + 1) {
@@ -35,14 +38,15 @@ void treatBoundary(double *collideField, int* flagField,
                         finv = collideField[Q * ((z+dz)*(N+2)*(N+2) + (x+dy)*(N+2) + y+dx) + Q-i-1];
                         collideField[Q * (z*(N+2)*(N+2) + x*(N+2) + y) + i] = finv;
                     }
-                    /*x-y plane swap x and z*/
+                    /*x-y plane swap x and z,the IF loop is therefore written within the same iteration,
+                    (same principle as x-z plane)*/
                     if (z+dx > 0 && z+dx < N + 1 &&
                         y+dy > 0 && y+dy < N + 1 &&
                         x+dz > 0 && x+dz < N + 1) {
                         
                         finv = collideField[Q * ((x+dz)*(N+2)*(N+2) + (y+dy)*(N+2) + z+dx) + Q-i-1];
                         
-                        
+                        /* Additionally, the BC-moving wall has to be considered*/ 
                         if(k == 1) {
                             currentCell = collideField + Q * ((x+dz)*(N+2)*(N+2) + (y+dy)*(N+2) + z+dx);
                             computeDensity(currentCell, &density);
