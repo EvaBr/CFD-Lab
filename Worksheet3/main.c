4100d9f34911306ee82459f347257313535e246c
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2015-06-02 01:01:35 +0200

    vorking version of main

--- Worksheet3/main.c
+++ Worksheet3/main.c
@@ -7,39 +7,6 @@
 #include <stdio.h>
 
 
-/**
- * The main operation reads the configuration file, initializes the scenario and
- * contains the main loop. So here are the individual steps of the algorithm:
- *
- * - read the program configuration file using read_parameters()
- * - set up the matrices (arrays) needed using the matrix() command
- * - create the initial setup init_uvp(), init_flag(), output_uvp()
- * - perform the main loop
- * - trailer: destroy memory allocated and do some statistics
- *
- * The layout of the grid is decribed by the first figure below, the enumeration
- * of the whole grid is given by the second figure. All the unknowns corresond
- * to a two dimensional degree of freedom layout, so they are not stored in
- * arrays, but in a matrix.
- *
- * @image html grid.jpg
- *
- * @image html whole-grid.jpg
- *
- * Within the main loop the following big steps are done (for some of the 
- * operations a definition is defined already within uvp.h):
- *
- * - calculate_dt() Determine the maximal time step size.
- * - boundaryvalues() Set the boundary values for the next time step.
- * - calculate_fg() Determine the values of F and G (diffusion and confection).
- *   This is the right hand side of the pressure equation and used later on for
- *   the time step transition.
- * - calculate_rs()
- * - Iterate the pressure poisson equation until the residual becomes smaller
- *   than eps or the maximal number of iterations is performed. Within the
- *   iteration loop the operation sor() is used.
- * - calculate_uv() Calculate the velocity at the next time step.
- */
 int main(int argn, char** args){
  	if (argn !=2 ) {
         	printf("When running the simulation, please give a valid file name for the problem!\n");
@@ -69,6 +36,9 @@ int main(int argn, char** args){
 	//read the parameters, using problem.dat, including wl, wr, wt, wb
 	read_parameters(filename, &Re, &UI, &VI, &PI, &GX, &GY, &t_end, &xlength, &ylength, &dt, &dx, &dy, &imax,
 			&jmax, &alpha, &omg, &tau, &itermax, &eps, &dt_value, &wl, &wr, &wt, &wb, problem, &presLeft, &presRight, &presDelta, &vel);
+
+	int pics = dt_value/dt; //just a helping variable for outputing vtk
+
 
 	//allocate memory, including Flag
 	U = matrix(0, imax+1, 0, jmax+1);
@@ -113,10 +83,8 @@ int main(int argn, char** args){
 		n++;
 		t += dt;
 		
-		//printf("into if");
 		//output of pics for animation
-		if (n % 20 == 0){
-		//	printf("works");
+		if (n%pics==0 ){
 			write_vtkFile("DrivenCavity", n, xlength, ylength, imax, jmax, dx, dy, U, V, P);  
 		}
 	}
