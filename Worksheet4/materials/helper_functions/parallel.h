55c780655f5e8e1ba14ab9e5b97e67fa9907b69b
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2015-06-16 01:11:36 +0200

    corrected input types for initialiseBuffers in .h

--- Worksheet4/materials/helper_functions/parallel.h
+++ Worksheet4/materials/helper_functions/parallel.h
@@ -16,4 +16,5 @@ void Programm_Sync(char *txt);
 void Programm_Stop(char *txt);
 /* all processes will produce a text output, be synchronized and finished */
 
-void initialiseBuffers( double *sendBuffer, double *readBuffer, int *xlength, int *proc);
+void initialiseBuffers( double **sendBuffer, double **readBuffer, int *xlength, int *proc);
+/* initialises buffers for one process */
