06f8eb96166d6214bde64918cc7954bb4f3ebee5
Wei Ni <paratroop@gmail.com>
2015-06-15 17:04:47 +0200

    for merging

--- Worksheet4/initLB.h
+++ Worksheet4/initLB.h
@@ -17,7 +17,7 @@ int readParameters(
 
 
 /* initialises the particle distribution functions and the flagfield */
-void initialiseFields(double *collideField, double *streamField,int *flagField, int *xlength, int ranks, int number_of_ranks);
+void initialiseFields(double *collideField, double *streamField,int *flagField, int *subdomain, int rank, int *proc);
 
 #endif
 
