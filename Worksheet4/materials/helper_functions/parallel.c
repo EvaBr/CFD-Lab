2d2ceb0f65a9172e26f4f842e69f3ec228d5f1ae
Wei Ni <paratroop@gmail.com>
2015-06-16 16:03:19 +0200

    first injection functions

--- Worksheet4/materials/helper_functions/parallel.c
+++ Worksheet4/materials/helper_functions/parallel.c
@@ -116,3 +116,39 @@ void extractionZ ( ){
 	//here we extract the five pdfs that would be streamed into our neighbour, z direction.
 	//TODO
 }
+
+//Injecion
+//here we inject the five pdfs that would be streamed into our neighbour, x direction.
+void injectionXright ( double **readBuffer, int rank, int *subdomain){
+	// 1. INJECT FROM RIGHT
+	// pdfs, that need to be extracted are the ones with indices 1, 5, 8, 11, 15.
+	int i = 0;
+	if (rank%subdomain[0]!=subdomain[0]-1){ //check if right boundary is not a no-slip
+		for (int z=0; z<subdomain[2]+2; z++){
+			for (int y=0; y<subdomain[1]+2; y++){
+				collideField [ Q*compute_index(1, y, z, subdomain) + 1 ] = *(readBuffer[1] + i++);
+				collideField [ Q*compute_index(1, y, z, subdomain) + 5 ] = *(readBuffer[1] + i++);
+				collideField [ Q*compute_index(1, y, z, subdomain) + 8 ] = *(readBuffer[1] + i++);
+				collideField [ Q*compute_index(1, y, z, subdomain) + 11 ] = *(readBuffer[1] + i++);
+				collideField [ Q*compute_index(1, y, z, subdomain) + 15 ] = *(readBuffer[1] + i++);
+			}
+		}
+	}
+}
+
+void injectionXleft ( double **readBuffer, int rank, int *subdomain){
+	// 2. INJECT FROM LEFT
+	// here, we extract pdfs with indices 3, 7, 10, 13, 17.
+	int i = 0;
+	if (rank%subdomain[0]!=0){ //check if left boundary is not a no-slip
+		for (int z=0; z<subdomain[2]+2; z++){
+			for (int y=0; y<subdomain[1]+2; y++){
+				collideField [ Q*compute_index(1, y, z, subdomain) + 3 ] = *(readBuffer[1] + i++);
+				collideField [ Q*compute_index(1, y, z, subdomain) + 7 ] = *(readBuffer[1] + i++);
+				collideField [ Q*compute_index(1, y, z, subdomain) + 10 ] = *(readBuffer[1] + i++);
+				collideField [ Q*compute_index(1, y, z, subdomain) + 13 ] = *(readBuffer[1] + i++);
+				collideField [ Q*compute_index(1, y, z, subdomain) + 17 ] = *(readBuffer[1] + i++);
+			}
+		}
+	}
+}
